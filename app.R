f6f1580870fbeff5a38a5b4aa89ddbafabdcc47b
ziyang06 <ziyang.soo@n-actuarial.com>
2023-12-25 15:29:22 +0800

    4th draft

--- app.R
+++ app.R
@@ -1,5 +1,9 @@
 library(shiny)
-library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(dplyr)
+library(DT)
+library(rsconnect)
 
 # Define the UI
 ui <- fluidPage(
@@ -16,7 +20,7 @@ ui <- fluidPage(
 #Design for main panel (in middle)
     mainPanel(
       tabsetPanel(
-        tabPanel("Cumulative Paid Claims Table", datatableOutput("table")),
+        tabPanel("Cumulative Paid Claims Table", dataTableOutput("table")),
         tabPanel("Cumulative Paid Claims Graph", plotOutput("graph"))
       )
     )
@@ -25,13 +29,12 @@ ui <- fluidPage(
 
 # Define the server logic
 server <- function(input, output) {
-  claims_data <- reactiveVal(NULL)
   
 # Create reactive for loss years from reading the input file
   loss_year <- reactive({
     req(input$file)
-    claims_data <- read_xlsx(input$file$datapath)
-    unique_loss_year <- unique(claims_data$`Loss Year`)
+    claims_data <- read_xlsx(input$file$datapath) 
+    unique_loss_year <- unique(claims_data$"Loss Year")
     return(unique_loss_year)
   })
   
@@ -41,19 +44,19 @@ server <- function(input, output) {
     claims_data <- read_xlsx(input$file$datapath)
 
   # Restructure table for cumulative paid claims    
-    paid_claims <- matrix(NULL, 
-                          nrow = length(loss_years()), 
-                          ncol = length(loss_years()) + 1)
+    paid_claims <- matrix(0, 
+                          nrow = length(loss_year()), 
+                          ncol = length(loss_year()) + 1)
     
-    cumulative_claims <- matrix(NULL, 
-                                nrow = length(loss_years()), 
-                                ncol = length(loss_years()) + 1)
+    cumulative_claims <- matrix(0, 
+                                nrow = length(loss_year()), 
+                                ncol = length(loss_year()) + 1)
     
-    for (i in 1:length(loss_years())) {
+    for (i in 1:length(loss_year())) {
       DY <- 1
-      for (j in 1:nrow(claims_table)) {
-        if (claims_table$'Loss Year'[j] == loss_years()[i]) {
-          paid_claims[i, DY] <- claims_table$'Amount of Claims Paid ($)'[j]
+      for (j in 1:nrow(claims_data)) {
+        if (claims_data$'Loss Year'[j] == loss_year()[i]) {
+          paid_claims[i, DY] <- claims_data$'Amount of Claims Paid ($)'[j]
           DY <- DY + 1
         } else {
           DY <- 1
@@ -61,7 +64,7 @@ server <- function(input, output) {
       }
 
   # Transform paid claims to cumulative paid claims     
-      for (j in 1:length(loss_years())) {
+      for (j in 1:length(loss_year())) {
         if (paid_claims[i, j] != 0) {
           if (j == 1) {
             cumulative_claims[i, j] <- paid_claims[i, j]
@@ -73,26 +76,26 @@ server <- function(input, output) {
     }
     
   # Calculate development factor for claims projection
-    factor <- matrix(1, nrow = 1, ncol = length(loss_years()) + 1)
-    for (i in 1:length(loss_years())) {
+    factor <- matrix(1, nrow = 1, ncol = length(loss_year()) + 1)
+    for (i in 1:length(loss_year())) {
       df1 <- 0
       df2 <- 0
       if (i != 1) {
-        df1 <- sum(cumulative_claims[1:(length(loss_years()) - i + 1), i - 1])
-        df2 <- sum(cumulative_claims[1:(length(loss_years()) - i + 1), i])
+        df1 <- sum(cumulative_claims[1:(length(loss_year()) - i + 1), i - 1])
+        df2 <- sum(cumulative_claims[1:(length(loss_year()) - i + 1), i])
         factor[1, i] <- df2 / df1
       }
     }
    
   # Assign the development factor for last development year to be Tail Factor input 
-    factor[1, length(loss_years()) + 1] <- input$tail_factor
+    factor[1, length(loss_year()) + 1] <- input$tail_factor
 
   # Perform projection    
     projected_claims <- cumulative_claims
-    for (i in 1:length(loss_years())) {
-      for (j in 1:length(loss_years()) + 1) {
-        if (is.null(projected_claims[i, j])) {
-          projected_claims[i, j] <- projected_claims[i, j - 1] * dev_factor[j]
+    for (i in 1:length(loss_year())) {
+      for (j in 1:length(loss_year()) + 1) {
+        if (projected_claims[i, j] == 0) {
+          projected_claims[i, j] <- projected_claims[i, j - 1] * factor[j]
         }
       }
     }
@@ -103,7 +106,7 @@ server <- function(input, output) {
   output$table <- renderDataTable({
     table <- as.data.frame(projection_data())
     DYlist <- c()
-    for (i in 1:(length(loss_years()) + 1)){
+    for (i in 1:(length(loss_year()) + 1)){
       DYlist <- append(DYlist, 
                               paste("Develpoment Year ", i, sep = ""))
     }
@@ -114,25 +117,25 @@ server <- function(input, output) {
   
 # Show output in graph
   output$graph <- renderPlot({
-    graph_data <- as.data.frame(stimulation_data())
+    graph_data <- as.data.frame(projection_data())
     
     DYlist1 <- c()
-    for (i in (1:(length(loss_years()) + 1))){
-      DYlist1 <- append(DYlist, i)
+    for (i in (1:(length(loss_year()) + 1))){
+      DYlist1 <- append(DYlist1, i)
     }
 
 # Show output in graph
     # Prepare data
     graph_data <- rbind(DYlist1, graph_data)
     transposed_graph_data <- as.data.frame(t(graph_data))
-    colnames(transposed_graph_data) <- c("Development Year", loss_years())
+    colnames(transposed_graph_data) <- c("Development Year", loss_year())
 
     # Fill in lines
     graph <- ggplot() + labs(x = "Development Year", y = "Cumulative Claims ($)")
-    for (i in 1:length(loss_years())){
+    for (i in 1:length(loss_year())){
       map1 <- aes_string(x = transposed_graph_data[,1],
                         y = transposed_graph_data[,(i+1)],
-                        color = factor(loss_years()[i]))
+                        color = factor(loss_year()[i]))
       graph <- graph + geom_smooth(map1,
                            method = "loess",
                            se = FALSE,
